1900e811e83ae33eaf5d6c64b5b9e5fa9710639e
boyani-Otieno <boyani.ombogo7@gmailcom>
2024-03-16 00:06:49 +0300

    Added Devise and font-awesome

--- Gemfile
+++ Gemfile
@@ -73,3 +73,7 @@ group :test do
   gem "selenium-webdriver"
 
 end
+
+gem "devise", "~> 4.9"
+
+gem "font-awesome-sass", "~> 6.5.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  #devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+   get "up" => "rails/health#show", as: "rails_healtyh_check"
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "home#index"
+
+   authenticated :admin_user do
+    root to: 'admin#index', as: :admin_root
+   end
+
+   get "admin" => "admin#index"
 end
